c21ca6d07a07a6444d4576680eb3e1099cfa5a01
rcatalan98 <robertitocatalan@gmail.com>
2020-10-10 18:19:22 -0300

    algo funca

--- coto_scraper/coto_scraper/spiders/name_price_spider.py
+++ coto_scraper/coto_scraper/spiders/name_price_spider.py
@@ -1,5 +1,15 @@
 import scrapy
 
-class namePriceSpider(scrapy.Spider) {
-    
-}
+class namePriceSpider(scrapy.Spider):
+    name = 'namePrice'
+    start_urls = [
+        'https://www.cotodigital3.com.ar/sitios/cdigi/browse/'
+    ]
+
+    def parse(self, response):
+        all_category_products = response.xpath('//*[@id="products"]')
+        for product in all_category_products:
+            name = product.xpath('//div[@class="descrip_full"]/text()').extract()
+            price = product.xpath('//span[@class ="atg_store_newPrice"]/text()').extract()
+            yield{'name': name,
+                  'price': price}
