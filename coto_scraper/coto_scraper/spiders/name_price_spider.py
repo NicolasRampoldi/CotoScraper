7c256855f29390b4ba15eb98d2dae380d04a356a
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2020-10-11 02:15:33 -0300

    No more repeated elements, missing discount prices

--- coto_scraper/coto_scraper/spiders/name_price_spider.py
+++ coto_scraper/coto_scraper/spiders/name_price_spider.py
@@ -7,12 +7,21 @@ class NamePriceSpider(scrapy.Spider):
     start_urls = [
         'https://www.cotodigital3.com.ar/sitios/cdigi/browse/'
     ]
-
+    #//span[@class="price_discount"]/text()
     def parse(self, response):
         all_category_products = response.xpath('//*[@id="products"]')
         for product in all_category_products:
             name = product.xpath('//div[@class="descrip_full"]/text()').extract()
-            price = product.xpath('//span[@class ="atg_store_newPrice"]/text()').re(r'\$\d{1,3}(?:[.,]\d{3})*(?:[.,'
-                                                                                    r']\d{2})')
+            price = product.xpath('//span[@class="atg_store_productPrice" and not(@style)]/span[@class '
+                                  '="atg_store_newPrice"]/text()').re(
+                r'\$\d{'
+                r'1,'
+                r'3}(?:['
+                r'.,'
+                r']\d{'
+                r'3})*('
+                r'?:[., '
+                r']\d{2})')
+
             yield {'name': name,
                    'price': price}
