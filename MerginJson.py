ba782138dd6d5d0324195b2303977eefaae1695e
rcatalan98 <robertitocatalan@gmail.com>
2020-10-25 22:52:52 -0300

    Se agrega la funcionalidad de subir a la base de datos los productos. Hay un problema que firebase corta la conexion luego de una cantidad de posts.

--- MerginJson.py
+++ MerginJson.py
@@ -1,11 +1,15 @@
 import json
-
+from firebase import firebase
+import re
+from time import sleep
+firebase = firebase.FirebaseApplication('https://listy-itba-app.firebaseio.com/')
 if __name__ == '__main__':
 
     with open('./coto_scraper/data.json') as file:
         data = json.load(file)
         products = []
     for page in data:
+
         names = page["name"]
         prices = page["price"]
         cant_names = len(names)
@@ -18,8 +22,12 @@ if __name__ == '__main__':
             cant_products = cant_prices
             make_change = True
         for i in range(cant_products):
-            product = {'name': names[i].replace('\n','').replace('\r','').replace('\t', ''), 'price': prices[i].replace('\n', '').replace('\r', '').replace('\t', '')}
+            true_name = re.sub(' +', ' ', names[i].replace('\n','').replace('\r','').replace('\t', '').replace(' . . .', '').replace(' . . . .','').rstrip())
+            true_name = re.sub('( \.)+', '', true_name)
+            product = {'name': true_name, 'price': prices[i].replace('\n', '').replace('\r', '').replace('\t', '')}
             products.append(product)
+            firebase.post('products', product)
+            sleep(15)
             print(product)
         if make_change:
             product = {'name': names[cant_names - 1], 'price': -1}
